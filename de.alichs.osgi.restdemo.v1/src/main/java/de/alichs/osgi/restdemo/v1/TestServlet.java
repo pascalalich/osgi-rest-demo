95547da7bfb2259d952e11b343cc9b5203d52ce6
pascalalich <pascal.alich@gmx.de>
2014-06-02 19:54:03 +0200

    Test servlet exported (Spring-powered)

---
+++ de.alichs.osgi.restdemo.v1/src/main/java/de/alichs/osgi/restdemo/v1/TestServlet.java
@@ -0,0 +1,24 @@
+package de.alichs.osgi.restdemo.v1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		resp.getOutputStream().println("Hello world");
+	}
+
+}
